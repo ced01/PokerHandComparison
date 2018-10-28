f3395e75a0bf61b0596e01164c0cbedd5b7441b3
cedric lautard <lautard1@hotmail.fr>
2018-10-28 21:21:11 +0100

    adding start command

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
-app.use("/src", express.static(__dirname + '/src'));
+app.use("/", express.static(__dirname + '/src'));
 
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/src', 'index.html'));
-});
+});*/
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
